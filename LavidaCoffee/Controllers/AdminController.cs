95eed5fe8508c7c48af07da4a2d0489510458a8a
Lambon <joe.lambon@ecclesiastical.com>
2025-02-01 16:57:11 +0000

    Fixed bug with admin data view switcher losing events table.

--- LavidaCoffee/Controllers/AdminController.cs
+++ LavidaCoffee/Controllers/AdminController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using LavidaCoffee.ViewModels;
 using System.Net;
+using Microsoft.IdentityModel.Tokens;
 
 namespace LavidaCoffee.Controllers
 {
@@ -24,7 +25,25 @@ namespace LavidaCoffee.Controllers
 			List<EmailRequest> emailRequests = _emailRequestRepository.AllEmailRequests();
 			IEnumerable<Event> upcomingEvents = _eventRepository.AllEvents.Where(e => e.Date > DateTime.Today);
 
+			if(!TempData.IsNullOrEmpty())
+			{
+				ViewBag.Message = TempData["errorMessage"].ToString();
+			}
 			return View(new AdminViewModel(upcomingEvents, emailRequests));
+		}
+
+		[Authorize(Roles = "Admin")]
+		[HttpPost]
+		public IActionResult DeleteEvent(int id)
+		{
+			var targetEvent = _eventRepository.GetEventById(id);
+			if(targetEvent!=null)
+			{
+				_eventRepository.DeleteEvent(targetEvent);
+				return RedirectToAction("Index");
+			}
+			TempData["errorMessage"] = $"Error: Failed to delete event - no event with id={id} found";
+			return RedirectToAction("Index");
 		}
 
 		[HttpPost]
